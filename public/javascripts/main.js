307f7390f010d880de4cdd8ba31963b0d50bf06b
bitchwhocodes <stacey.mulcahy@gmail.com>
2016-02-02 11:13:26 -0500

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+
+    var c = document.getElementById('photo');
+    var v = document.getElementById('camera');
+    var context = c.getContext('2d');
+    $('#sendbutton').click(function(eObject){
+        sendImage();
+    })
+
+
+    navigator.getUserMedia = (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+    if (navigator.getUserMedia) {
+        // Request access to video only
+        navigator.getUserMedia(
+            {
+                video: true,
+                audio: false
+            },
+            function (stream) {
+                var url = window.URL || window.webkitURL;
+                v.src = url ? url.createObjectURL(stream) : stream;
+                v.play();
+                var c = document.getElementById('photo');
+                console.log(c);
+                var context = c.getContext('2d');
+                context.drawImage(v, 0, 0, c.width, c.height);
+
+            },
+            function (error) {
+                alert('Something went wrong. (error code ' + error.code + ')');
+                return;
+            }
+            );
+    }
+    else {
+        alert('Sorry, the browser you are using doesn\'t support getUserMedia');
+        return;
+    }
+
+    function sendImage() {
+        console.log("send image");
+        //var c = document.getElementById('photo');
+        var url = c.toDataURL('image/jpeg');
+        console.log(url);
+        $.ajax({
+            url: '/image',
+            type: 'PUT',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({ data: url }),
+            success: function () { console.log("oh noes") }
+        });
+    }
+    function showImage() {
+        context.drawImage(v, 0, 0, c.width, c.height);
+    }
+
+    setInterval(showImage, 1);
+});
+
+
+
+
+
+
+
+
+
---
+++ routes/image.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+var Spark = require("spark");
+var oxfordEmotion = require("node-oxford-emotion")(process.env.PO_KEY);
+var fs = require('fs');
+var sys = require('sys');
+
+
+
+function parseDataURL(body) {
+  var match = /data:([^;]+);base64,(.*)/.exec(body);
+  if(!match)
+    return null;
+
+  return {
+    contentType: match[1],
+    data: new Buffer(match[2], 'base64')
+  };
+}
+
+/* GET image data here */
+router.put('/', function(req, res, next) {
+req.body.data = req.body.data.replace(/^data:image\/jpeg+;base64,/, "");
+req.body.data = req.body.data.replace(/ /g, '+');
+var path = require('path'); 
+var url = path.join(process.cwd(), 'public') 
+console.log(url)
+fs.writeFile(url+'/images/image.jpeg', req.body.data, 'base64', function(err) {
+    oxfordEmotion.recognize("url", req.body.data, function(response) {
+        console.log(response);
+        res.json(response);
+    });
+ 
+});
+
+
+
+
+
+
+
+  
+});
+
+/*
+function decodeBase64Image(dataString) 
+        {
+          var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+          var response = {};
+
+          if (matches.length !== 3) 
+          {
+            return new Error('Invalid input string');
+          }
+
+          response.type = matches[1];
+          response.data = new Buffer(matches[2], 'base64');
+
+          return response;
+        }
+
+function getItemEmotionRating(data)
+{
+  console.log("get emotion rating",data);
+  var promise = new Promise(function(resolve, reject) {
+ 
+    oxfordEmotion.recognize("url", data, function(cb) {
+     
+  
+      resolve(data);
+    });
+  });
+  return promise;
+}
+
+*/
+
+module.exports = router;
